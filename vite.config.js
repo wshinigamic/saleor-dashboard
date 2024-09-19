15a08ced7eddf434f47ef7d2d15c1854561846eb
Gan <wshinigamic@hotmail.com>
2024-09-19 18:11:31 +0800

    Change build dir

--- vite.config.js
+++ vite.config.js
@@ -185,7 +185,7 @@ export default defineConfig(({ command, mode }) => {
       sourcemap,
       minify: false,
       emptyOutDir: true,
-      outDir: "../build",
+      outDir: "dist",
       // outDir: "../build/dashboard",
       assetsDir: ".",
       commonjsOptions: {
